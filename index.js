d959ac4d2859605bad14d799af2480a3008054a8
Nana <72888369+nanaama527@users.noreply.github.com>
2021-07-04 16:29:38 -0400

    added css formatting

---
+++ index.js
@@ -0,0 +1,5 @@
+// Baseline Build - Recommended start up
+document.addEventListener("click", (event)=>{ console.log( "You Just Clicked on == ", event.target) } )
+
+///
+document.addEventListener("DOMContentLoaded", function(){ console.log("It's Lit!") })
